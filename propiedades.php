ad3b60495246aad16fec943f9934a5c6cb97f8ec
bianca92 <bianneco92@gmail.com>
2019-05-08 19:41:18 -0300

    formulario para crear propiedad

---
+++ propiedades.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		
+<?php
+		include("cabecera.php");
+		include("clases.php");
+		include("conexion.php");
+		
+?>
+	<title>Propiedades</title>
+	
+	</head>
+	<body>
+		
+		<div id="wrapper">
+
+			<form name="formulario" action="altapropiedad.php" method="POST" class="login-form" enctype="multipart/form-data" onsubmit="return validareg();">
+				
+				<div class="header">
+					<h1>  Propiedades</h1>
+				</div>
+			 
+				
+				<div class="content">
+					<label for="titulo">Titulo: </br></label>
+					<input type="text" name="titulo" class="input username" size="25"  pattern="{2,20}">
+
+					<label for="ubicacion">Ciudad: </br></label>
+					<input type="text" name="ubicacion" class="input username" maxlength="8" required="required">
+					
+					<label for="descripcion">Descripcion: </br></label>
+					 <textarea name="descripcion" rows="4" cols="50"></textarea></br></br>
+					
+					
+					<label for="imagen">Imagen: </br></label>
+					<input type="file" name="imagen" id="imagen" accept="image/*" required="required" >
+					
+					
+					
+				</div>
+				<div class="footer">
+					<input type="submit" name="login" value="Dar de alta propiedad" class="button" />
+				</div>
+			</form>
+			
+		</div>
+	</body>
+</html>

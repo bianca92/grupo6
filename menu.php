c9fa1ec2eb5ac6fa8792319a0cc506cd3c7a7596
bianca92 <bianneco92@gmail.com>
2019-05-08 19:35:29 -0300

    archivo sobre menu de la pagina

---
+++ menu.php
@@ -0,0 +1,51 @@
+
+<html>
+
+	<div id="menu">
+             
+                 <ul class="nav nav-pills">
+                  <li><a href="listarPropiedades.php">PROPIEDADES</a></li>
+             	         
+    <?php
+			if(isset($_SESSION['estado'])){
+      	  		 if($_SESSION['estado']=="logeado"){
+
+                        if($_SESSION['rol']=="1"){
+                       ?>	
+                        	
+                          <li class="active"><a href="propiedades.php">Agregar</a></li>
+                          <li><a href="listarSubastas.php">SUBASTAS</a></li>
+						              <li class="active"><a href="altaSubasta.php">Agregar</a></li>
+                                            
+                             
+		 <?php
+                        }
+
+
+                       else{
+           ?>
+						
+				               
+                       <li><a href="listarSubastas.php">SUBASTAS</a></li>
+						           <li><a href="misSubasta.php">MIS SUBASTAS</a></li>
+                                            
+    <?php						  		
+			                }
+		            }
+	            }
+			    
+
+            ?>
+              </ul>
+           </div>
+         <div class="clearfix visible-lg"></div>       
+	
+      <script src="js/jquery.js"></script>
+	  <script src="js/bootstrap.min.js"></script>
+	  <script src="js/bootstrapValidator.min.js"></script>
+
+
+
+
+
+</html>

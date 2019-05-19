3f99a1dc83e27d30b9e8d2a554f8366be153e1a9
bianca92 <bianneco92@gmail.com>
2019-05-19 16:13:46 -0300

    cambios en le form

--- alta_subasta.php
+++ alta_subasta.php
@@ -1,10 +1,10 @@
-		
+<html>
+   <head>		
 <?php
 		
 		include("clases.php");  
     include("cabecera.php");
 include("conexion.php");
-
 
 
 //$link = mysqli_connect('localhost','root','','grupo6');
@@ -13,13 +13,9 @@ $link=conectar();
 $Propiedad=$_GET['no'];
 
 
-
 $var_consulta= "SELECT * FROM propiedad WHERE idPropiedad='$Propiedad' ";
 $var_resultado = $link->query($var_consulta);
 $row = mysqli_fetch_array($var_resultado);
-
-
-
  
 ?>
 	<title>SUBASTA</title>
@@ -38,44 +34,41 @@ $row = mysqli_fetch_array($var_resultado);
 				
 				<div class="content">
 
-					
 					<input type="hidden" name="propiedad" value='<?php echo "$row[0]" ?>' size="25">
-                <div>
+
+                    <div>
 					<label for="año">AÑO: </br></label>
-					<input type="text" name="year" size="8" min="2019" >
+					<input id="fecha"   type="text" name="year" class="input username" size="8" min="2019" >
 					
-					  SEMANA:
-                               <select name="semana">
+					<label for="semana">SEMANA:</label>
+                    <select id="semana"  name="semana">
                                
-            <?PHP              
+                    <?PHP              
                       for ($i=1; $i < 54; $i++) { 
             	
                               echo" <option value= $i selected>$i</option>";
-             }
-            ?>
-                               </select></br>
-                    </div></br>          
-
-					<div>
+                    }
+                    ?>
+                    </select>
+                    </div></br>
+                
+                    <div>
 					<label for="precioInicial">Precio Inicial: </br></label>
-					<input type="number" name="precioInicial"  ></div></br>
+					<input id="precio" type="number" name="precioInicial"  ></div></br>
 					<div>
-					<label for="insDesde">Inscripcion
-					 desde : </br></label>
-					<input type="date" name="insDesde" >
+					<label for="insDesde">Inscripcion desde : </label>
+					<input id="fecha" type="date" name="insDesde">
+					
+					<label for="insHasta">hasta : </label>
+					<input id="fecha" type="date" name="insHasta" >
 					</div></br>
 					<div>
-                    <label for="insHasta">hasta : </br></label>
-					<input type="date" name="insHasta" >
-					</div></br>
+                    <label for="ofDesde">Ofertar desde : </br></label>
+					<input id="fecha" type="date" name="ofDesde" >
+					</div>
 					<div>
-                    <label for="ofDesde">Ofertar
-                     desde : </br></label>
-					<input type="date" name="ofDesde" >
-					</div></br>
-					<div>
-                    <label for="ofHasta">hasta : </br></label>
-					<input type="date" name="ofHasta" >
+                    <label for="ofHasta">hasta : </label>
+					<input id="fecha" type="date" name="ofHasta" >
 					</div>
                     
 					

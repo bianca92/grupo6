e4f8b8e7403393b0cf47fe9c771863945688823a
bianca92 <bianneco92@gmail.com>
2019-05-21 16:00:35 -0300

    se modifico q no siempre se muestren las opciones

--- cabecera.php
+++ cabecera.php
@@ -21,27 +21,29 @@
 <body>
 	<div id="header">
 
-	<?php   $archivop=""; $archivosu=""; $archivoActUsu=""; $archivoTerminadas=""; 
+	<?php   $archivop=""; $archivosu=""; $archivoActUsu=""; $archivoTerminadas=""; $verOpcionesLogeado=false;
              if(isset($_SESSION['estado'])){
       	  		 if($_SESSION['estado']=="logeado"){
-
+                        
+                        //ADMINISTRADOR
                         if($_SESSION['rol']=="1"){
                         	//echo"<h3><a href=index.php><span class= 'glyphicon glyphicon-home'</span></a> | 
       	           	         //    HOLA ADMINISTRADOR ".$_SESSION['nombre']." ! | <a href=salir.php><span class='glyphicon glyphicon-log-out'></span>CERRAR SESIÓN</a></h3>";
       	           	             $opcion1="<a href'#'><span class'glyphicon-user'></span>Hola Administrador!</a>";
                                   $opcion2="<a href=salir.php><span class='glyphicon glyphicon-log-out'></span>Salir</a>";  
                                   $archivop="propiedadesAdmin.php"; $archivosu="subastasAdmin.php"; $archivoActUsu="subastasActivasAdministrador2.php"; $archivoTerminadas="subastasTerminadasAdministrador2.php";
-        
+                                  $verOpcionesLogeado=true;
                         }
                       else {
-
+                      // USUARIO REGISTRADO
 						
-				      //echo" <h3><a href=index.php><span class= 'glyphicon glyphicon-home'</span></a> | 
+				                  //echo" <h3><a href=index.php><span class= 'glyphicon glyphicon-home'</span></a> | 
 	      	           	   //  HOLA ".$_SESSION['nombre']." ! | <a href=salir.php><span class='glyphicon glyphicon-log-out'></span>CERRAR SESIÓN</a></h3>";
-	                             $opcion1="  Hola ".$_SESSION['nombre']." !  ";  
+	                             $opcion1="<a href'#'><span class'glyphicon-user'></span>Hola $_SESSION[nombre] !</a>" ;  
                                $opcion2="<a href=salir.php><span class='glyphicon glyphicon-log-out'></span>Salir</a>";
 	                             $archivop="listarPropiedades.php"; $archivosu="subastasUsuario.php"; $archivoActUsu="subastasActivasUsuario4.php"; $archivoTerminadas="subastasTerminadasUsuario.php";
-			                }
+			                         $verOpcionesLogeado=true;
+                      }
 		            }
 	            }
 			    else{ 
@@ -52,19 +54,25 @@
             ?>		
 
 
-
 <nav class="navbar navbar">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#"><img src="imgs/HSH-Complete.svg" class="img-thumbnail" width="110" height="100" margin-bottom: -60px></a>
     </div>
+
+    <?php // Mostra solo si esta logueado
+    if($verOpcionesLogeado==true){                                   ?>
     <ul class="nav navbar-nav">
       
       <li><a href=<?php echo $archivop; ?>>PROPIEDADES</a></li>
       <li><a href=<?php echo "$archivosu"; ?>>SUBASTAS</a></li>
       <li><a href=<?php echo $archivoActUsu; ?>>SUBASTAS ACTIVAS</a></li>
       <li><a href=<?php echo $archivoTerminadas; ?>>SUBASTAS TERMINADAS</a></li>
+
     </ul>
+   <?php  }
+     //Siempre se muestan estas opciones
+     ?>
     <ul class="nav navbar-nav navbar-right">
       <li><?php echo"$opcion1"; ?></li>
       <li><?php echo"$opcion2";?></li>

2e54c5ed47ccd1e2236562e989372226748dc87d
bianca92 <bianneco92@gmail.com>
2019-06-17 12:20:10 -0300

    se redirecciona a diferentes paginas

--- busqueda.php
+++ busqueda.php
@@ -8,7 +8,7 @@ $nuevafecha = date ( 'Y-m-d' , $nuevafecha );
           
  
 ?>
-<form method="GET" action="listarPropiedades.php" >
+<form method="GET" action= <?php $pagina.".php" ?>>
  <p></i><input type="text" name="lugar" required="required" placeholder="Ingrese lugar" />
  </i><input type="date" name="inicio" min='<?php echo $nuevafecha; ?>' required="required" />
  </i><input type="date" name="fin" min='<?php echo $nuevafecha; ?>' required="required"/>

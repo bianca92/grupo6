0c2f244a5e76f643639f145263826425d55214cc
bianca92 <bianneco92@gmail.com>
2019-06-17 12:22:26 -0300

    se guardan los datos de la valoracion

---
+++ calificar2.php
@@ -0,0 +1,38 @@
+<?php	
+	include("clases.php");  
+    include("cabecera.php");
+    include("conexion.php");
+
+
+$link=conectar();
+ $id=($_SESSION['id']);
+ $fecha=date('Y-m-j-H:i');
+
+$propiedad=$_POST['propiedad'];
+
+$subasta=$_POST['subasta'];
+$estrellas=$_POST['estrellas'];
+
+$opinion=$_POST['opinion'];
+
+
+$var_consulta="INSERT INTO valoracion (comentario,puntuacion, idPropiedad,idPersona,fecha, idSubasta)
+                     values('$opinion','$estrellas','$propiedad','$id','$fecha','$subasta')";
+            	
+$var_resultado = $link->query($var_consulta);
+
+
+
+
+
+
+ mysqli_close($link);
+
+
+
+
+header("Location:subastasGanadasUsuario.php");
+
+ 
+
+?>

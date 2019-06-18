60c5f32c88965528037e8627ef76d2acb4697b99
bianca92 <bianneco92@gmail.com>
2019-06-18 18:21:04 -0300

    nuevas busqueda y propiedades eliminadas

--- propiedadesAdmin.php
+++ propiedadesAdmin.php
@@ -34,16 +34,16 @@ $fecha_actual = date('Y-m-d');
 
 
   <div class="container">
-
+   <div class="col-sm-4" > 
     <a href="propiedades.php" class="btn btn-warning" float="left">NUEVO</a>
-
-
+   </div>
+   <div class="col-sm-8" >
     <!-- EL FORM DE LA BUSQUEDA -->
-    <form method="GET" action="propiedadesAdmin.php" >
- <p></i><input type="text" name="lugar" required="required" placeholder="Ingrese titulo o lugar" />
- <input type="submit" value="Buscar"/> 
-</form>
-
+    <form class="form-inline" method="GET" action="propiedadesAdmin.php" >
+     <p></i><input type="text" class="form-control"  name="lugar" required="required" placeholder="Ingrese titulo o lugar" />
+     <input type="submit" class="btn btn-default" value="Buscar"/> 
+   </form>
+ </div>
 <?php
 
 
@@ -85,43 +85,46 @@ $filtro=$_GET['lugar'];
             
            //si se recibio GET pero no hay propiedad
             $muestra=false;
-          
-        
-    
-           // busco si el lugar o titulo ingresado se encuentra entre la info de la propiedad  
+          // busco si el lugar o titulo ingresado se encuentra entre la info de la propiedad  
             $ubicacion1 = stripos($row['pais'], $filtro);
              $ubicacion2 = stripos($row['provincia'], $filtro);
               $ubicacion3 = stripos($row['localidad'], $filtro);
              $titulo = stripos($row['titulo'], $filtro);
           
-                
-          
-         if($ubicacion1!==false or $ubicacion2!==false or $ubicacion3!==false or $titulo!==false){
-          
-          $muestra=true;
-         
-                    }
-       
-      
+            if($ubicacion1!==false or $ubicacion2!==false or $ubicacion3!==false or $titulo!==false){
+                    $muestra=true;
+            }
       }
 
      else {//si no se recibio nada por GET que me muestre todo
  
-             $muestra=true;
-      }
+             $muestra=true; 
+    }
 
-      if($muestra==true){
+    if($muestra==true){
      $auxiliar=true;
 
       $imgs=ObtenerImgs($row['idPropiedad']); ?>
       <tr>
-            <td><?php echo '<img src="data:image/jpeg;base64,'.base64_encode($imgs[0]).'" style=width:20% />';?></td>
+  <?php if ($row['eliminada']==1) {
+        echo'<td><img src="data:image/jpeg;base64,'.base64_encode($imgs[0]).'" style=width:20% /></td>';} 
+        else{ ?>
+            <td><?php echo
+            "<a href='detalleAdmin.php?prop=$row[idPropiedad]'>". 
+            '<img src="data:image/jpeg;base64,'.base64_encode($imgs[0]).'" style=width:20% /></a>';?></td>
+   <?php  } ?>
             <td><h4><?php echo "$row[titulo]" ?></h4> </td>
-             <td><h4><?php echo" $row[pais] ";?></h4></td>
-              <td><h4><?php echo" $row[provincia] ";?></h4></td>
+            <td><h4><?php echo" $row[pais] ";?></h4></td>
+            <td><h4><?php echo" $row[provincia] ";?></h4></td>
             <td><h4><?php echo" $row[localidad] ";?></h4></td>
+
+  <?php if ($row['eliminada']==1) {
+              echo"<td></td><td>ELIMINADA</td><td></td>";}       
+        else{ ?>
             <td><?php echo "<a href='modificar_propiedad.php?no=".$row[0]."'> <button type='button' class='btn btn-succes'>MODIFICAR</button> </a>" ;?></td>
             <td><?php echo "<a href='alta_subasta.php?no=".$row[0]."'> <button type='button' class='btn btn-succes'>SUBASTAR</button> </a>" ;?></td>
+            <td><?php echo "<a href='eliminar_propiedad.php?no=".$row['idPropiedad']."'> <button type='button' class='btn btn-succes'>ELIMINAR</button> </a></br>" ;?></td>
+             <?php } ?>  
          </tr>  
          <?php } 
 

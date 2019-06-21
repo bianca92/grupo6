16a1a7f0f619e85816dcd5a1284bb9a38c2caa48
VirginiaMagne <virmagne94@gmail.com>
2019-06-21 07:16:05 -0300

    popup leyenda

--- cabecera.php
+++ cabecera.php
@@ -20,6 +20,143 @@
     <meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>
 	<style type="text/css"></style>
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+/* Popup container - can be anything you want */
+.popup {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* The actual popup */
+.popup .popuptext {
+  visibility: hidden;
+  width: 1000px;
+  height: 170px;
+  background-color: #A8E0FE;
+  color: #000000;
+  text-align: justify;
+  border-radius: 6px;
+  padding: 8px 0;
+  position: absolute;
+  z-index: 1;
+  top: 50px;
+  bottom: 125%;
+  left: 100%;
+  margin-left: -1100px;
+}
+
+/* Popup arrow */
+.popup .popuptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Toggle this class - hide and show the popup */
+.popup .show {
+  visibility: visible;
+  -webkit-animation: fadeIn 1s;
+  animation: fadeIn 1s;
+}
+
+/* Add animation (fade in the popup) */
+@-webkit-keyframes fadeIn {
+  from {opacity: 0;} 
+  to {opacity: 1;}
+}
+
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity:1 ;}
+}
+
+.popup:hover {
+  color: #FFFFFF;
+}
+
+
+</style>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+/* Popup container - can be anything you want */
+.popup2 {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* The actual popup */
+.popup2 .popuptext2 {
+  visibility: hidden;
+  width: 1000px;
+  height: 170px;
+  background-color: #A8E0FE;
+  color: #000000;
+  text-align: justify;
+  border-radius: 6px;
+  padding: 8px 0;
+  position: absolute;
+  z-index: 1;
+  top: 50px;
+  bottom: 125%;
+  left: 100%;
+  margin-left: -800px;
+}
+
+/* Popup arrow */
+.popup2 .popuptext2::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Toggle this class - hide and show the popup */
+.popup2 .show2 {
+  visibility: visible;
+  -webkit-animation: fadeIn 1s;
+  animation: fadeIn 1s;
+}
+
+/* Add animation (fade in the popup) */
+@-webkit-keyframes fadeIn {
+  from {opacity: 0;} 
+  to {opacity: 1;}
+}
+
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity:1 ;}
+}
+
+.popup2:hover {
+  color: #FFFFFF;
+}
+
+
+</style>
+
  
 
 
@@ -59,6 +196,9 @@
 			    else{ 
 			    	//echo"<h3><a href=index.php><span class= 'glyphicon glyphicon-home'</span></a> | <a href=ingresar.php><span class='glyphicon glyphicon-log-in'></span> INGRESAR</a> |<a href=registrar.php><span class='glyphicon glyphicon-user'></span> REGISTRARSE</a></h3>";}
                         $opcion1="<a href=ingresar.php><span class='glyphicon glyphicon-log-in'></span> INGRESAR</a>"; 
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
                         $opcion2="<a href=registrar.php><span class='glyphicon glyphicon-user'></span> REGISTRARSE</a>"; 
                     }
             ?>		
@@ -99,8 +239,32 @@
                      WHERE idPersona='".$_SESSION['id']."'";
            $var_resultado = mysqli_query(mysqli_connect('localhost','root','','grupo6'),$var_consulta);
            $num=mysqli_num_rows($var_resultado);  
-           if ($num==0){?>
-                 <li><a href=<?php echo $premium; ?>>¡QUIERO SER PREMIUM!</a></li>
+           if ($num==0){
+
+            $sql = "SELECT * FROM config_cuota WHERE idTipoUsuario='2'";
+  $res = mysqli_query(mysqli_connect('localhost','root','','grupo6'),$sql);
+  $montoPremium = mysqli_fetch_array($res);
+//-------------------------------------------------------------------------------------------------------------------------------------------?>
+              <div class="popup2" onclick="myFunction2()"> ¡QUIERO SER PREMIUM!
+              <span class="popuptext2" id="myPopup2"><ul  style="list-style-type:disc;" >
+      <h6><li>Como usuario premium podra comprar de forma directa semanas que se vayan a subastar sin la necesidad de pujar.</li>
+        <li>Los usuarios premium cuentan con dos creditos anuales, como el usuario clasico. Estos creditos representan una semana al año cada uno, por lo que se descontara un credito</li> al ganar una subasta y un credito al realizar una compra directa.<br/>
+        <li>Como usuario premium los creditos se pueden usar de cualquiera de las siguientes combinaciones:</li>
+          <ul style="list-style-type: disc;">
+            <li>Los dos en dos subastas.</li>
+            <li>Los dos en compras directas.</li>
+            <li>Uno en compra directa y otro en subasta.</li>
+          </ul>
+        <li>Los usuarios premium podran dar de baja este beneficio y volver a ser usuarios clasicos en cualquier momento.</li>
+        <li>El costo de ser usuario premium es de $ <?php echo "$montoPremium[monto]" ?> por mes.</li>
+      </br>
+      <?php echo"<a href=solicitarPremium.php> ¡SER PREMIUM!</a>";?>
+
+      </h6>  
+      </ul></span>
+</div>
+
+                
            <?php } 
            else{ ?>
                 <li><a href=#>SOLICITUD ENVIADA</a></li>
@@ -165,10 +329,36 @@
       }
         
 if($verOpcionesLogeado!=true){
+
+  $sql = "SELECT * FROM config_cuota WHERE idTipoUsuario='1'";
+  $res = mysqli_query(mysqli_connect('localhost','root','','grupo6'),$sql);
+  $montoClasico = mysqli_fetch_array($res);
+
+
+  
+
      ?>
   
-      <li><?php echo"$opcion1"; ?></li>
-      <li><?php echo"$opcion2";?></li>
+      <li><?php echo"$opcion1"; ?></li> <?php
+//----------------------------------------------------------------------------------------------------------------------------------------?>
+<div class="popup" onclick="myFunction()"><p class='glyphicon glyphicon-user'> </p> REGISTRARSE
+  <span class="popuptext" id="myPopup"><ul  style="list-style-type:disc;" >
+      <h6><li>Como usuario registrado poseera dos creditos anuales, los cuales representan una semana al año cada uno.</li>
+        <li>Podra comprar semanas en cualquiera de las distintas propiedades que se encuentran disponibles en el sitio.</li>
+        <li>La venta de semanas es mediante subastas, en las cuales debera inscribirse para poder pujar y si realiza la puja mas alta al cierre de la misma se le otorgara la semana subastada.</li>
+        <li>La semana ganada se podra abonar con la tarjeta registrada o con cualquier otra tarjeta. El valor de la misma es el ultimo monto pujado.</li>
+        <li>Cada semana ganada mediante subasta equivale a un credito, por lo que podra comprar mediante subasta solo dos semanas al año.</li>
+        <li>Como usuario registrado podra acceder al servicio premium, el cual le dara el beneficio de comprar semanas que se vayan a subastar de forma directa sin la necesidad de pujar. </li>
+        <li>Este sitio esta adherido al Hot Sale, por lo que habran semanas en distintas propiedades disponibles para ser adquiridas mediante compra directa en momento de Hot Sale.</li>
+        <li>Las compras en Hot Sale no descuentan creditos.</li>
+        <li>El costo de ser usuario clasico es de $ <?php echo "$montoClasico[monto]" ?> por mes.</li>
+      </br>
+      <?php echo"$opcion2";?>
+
+      </h6>  
+      </ul></span>
+</div>
+       
     </ul>
     <?php
       }
@@ -182,6 +372,21 @@ if($verOpcionesLogeado!=true){
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/bootstrapValidator.min.js"></script>
+    <script>
+      // When the user clicks on div, open the popup
+      function myFunction() {
+          var popup = document.getElementById("myPopup");
+          popup.classList.toggle("show");
+      }
+    </script>
+        <script>
+      // When the user clicks on div, open the popup
+      function myFunction2() {
+          var popup2 = document.getElementById("myPopup2");
+          popup2.classList.toggle("show2");
+      }
+    </script>
+
 
 
 </body>

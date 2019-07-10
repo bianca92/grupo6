ed1c30a909ce5285ef3deef19b18a6f086922278
bianca92 <bianneco92@gmail.com>
2019-07-10 17:45:54 -0300

    se cambio la configuracion

--- configuracionHotSale.php
+++ configuracionHotSale.php
@@ -5,7 +5,7 @@
 		
 		include("clases.php");  
     include("cabecera.php");
-include("conexion.php");
+    include("conexion.php");
 
 
 
@@ -24,7 +24,8 @@ $var_resultado = $link->query($var_consulta);
 
 
 
-	<title>Configuracion cuotas</title>
+
+	<title>Configuracion hotsale</title>
 	
 	</head>
 
@@ -36,31 +37,15 @@ $var_resultado = $link->query($var_consulta);
     $resu2 = $link->query($consulta); 
     $row2=mysqli_fetch_array($resu2);
 
-    //CREO LA FECHA DEL HOTSALE DE ESTE AÑO
-    $añoH=date("Y");
-    $fecha="$row2[dia]-$row2[mes]-$añoH";
-    $fechaHotsale= strtotime ( 'd-m-Y' , strtotime ( $fecha ) ) ;
-    $fechaH=strtotime($fechaHotsale);
 
-    //me fijo que el hotsale de este año no haya pasado
-    $fecha_actual=date('d-m-Y');
 
-    if ($fecha_actual > $fechaHotsale){
-       $añoH=$añoH + 1;
-       $fecha="$row2[dia]-$row2[mes]-$añoH";
-       //creo la fecha definitiva del hotsale
-       $fechaHotsale= date ( 'd-m-Y' , strtotime ( $fecha ) ) ;
-       //la paso a un formato comparable
-       $fechaH=strtotime($fechaHotsale);
-
-    }
-    $fechaH=date('d/m/Y', $fechaH);
+    $fechaHotsale="$row2[dia]/$row2[mes]/$row2[year]";
 
     ?>
 		
 		
 		<div id="wrapper">
-      <h4 style='color:#FF7516'>El proximo Hot Sale sera el <?php echo"$fechaH";?> </h4>
+      <h4 style='color:#FF7516'>El proximo Hot Sale sera el <?php echo"$fechaHotsale";?> </h4>
 
 
 			<form name="formulario" action="configuracionHotSale_2.php" method="POST" class="login-form" enctype="multipart/form-data" onsubmit="return validareg();">
@@ -77,15 +62,14 @@ $var_resultado = $link->query($var_consulta);
                   $primeraVez=1;
                   $duracion="";
                   $fecha="";
-                  ?>
-
-                <?php
+                  
                    }
                else {
-               $primeraVez=0;
+               $primeraVez=0;   
                $row = mysqli_fetch_array($var_resultado);
-               $fecha="$row[dia] - $row[mes]";
-               $duracion=$row['duracion'];
+               $duracion="$row[duracion]";
+             
+             
 
               ?>
 
@@ -102,11 +86,11 @@ $var_resultado = $link->query($var_consulta);
                     <input type="text" name="primera" class="hidden" value='<?php echo "$primeraVez" ?>'>
 				
 					<label for="date">Inicio: </br></label>
-					<input id="datepicker"   type="text" name="datepicker" class="input username" placeholder='<?php echo "$fecha" ?>' size="8" autocomplete="off" required="required">
+					<input id="datepicker"   type="text" name="datepicker" class="input username" value='<?php echo "$row[fecha]" ?>' size="8" autocomplete="off" required="required">
 
 
                    <label for="duracion">Dias de duracion: </br></label>
-					<input id="duracion"   type="number" name="duracion" class="input username" min=1 placeholder='<?php echo "$duracion" ?>' autocomplete="off" >
+					<input id="duracion"   type="number" name="duracion" class="input username" min=1 value='<?php echo "$duracion" ?>' autocomplete="off" >
 					
                 
 
@@ -138,14 +122,14 @@ $var_resultado = $link->query($var_consulta);
 
             $.datepicker.setDefaults($.datepicker.regional["es"]);
             $( "#datepicker" ).datepicker({
-            	defaultDate: '01-01',
-                dateFormat: 'dd-mm',
+            	defaultDate: '2020-01-01',
+                dateFormat: 'yy-mm-dd',
                 changeMonth:true,
                 changeYear:true,
-                yearRange: "2019:2019", 
+                yearRange: "2019:2021", 
                 firstDay: 1,
                 minDate: new Date('2019-01-01'), /** AAAA,MM,DD Fecha inicio */
-                maxDate: new Date('2020-01-01'), /** AAAA,MM,DD Fecha Fin */
+                maxDate: new Date('2021-01-01'), /** AAAA,MM,DD Fecha Fin */
 
                 monthNames: ['Enero', 'Febrero', 'Marzo',
                 'Abril', 'Mayo', 'Junio',

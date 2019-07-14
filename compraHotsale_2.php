a984c870fc4d84a39d79a516b53e02f7c66e377b
bianca92 <bianneco92@gmail.com>
2019-07-14 16:04:07 -0300

    cambios

--- compraHotsale_2.php
+++ compraHotsale_2.php
@@ -10,17 +10,12 @@ include("cabecera.php");
  $persona=$_POST['persona'];
  $tarjeta=$_POST['tarjeta'];
  $monto=$_POST['monto'];
- //$tipo=$_POST['tipo'];
+ 
 
  $fecha=date('Y-m-j-H:i');
 
 
-//$consulta= "SELECT * FROM persona WHERE idPersona='$persona'";
-//$var_resultado = $link->query($consulta);
-//$row = mysqli_fetch_array($var_resultado);
-//$creditos= $row['credito'] -1;
-//$var_consulta="UPDATE persona SET credito='$creditos' WHERE IdPersona='$ganador' ";           	
-//$var_resultado = $link->query($var_consulta);
+
  $var_consulta="INSERT INTO compraH (idPersona,idHotsale,idTarjeta,monto,fecha)
                      values('$persona','$hotsale','$tarjeta','$monto', '$fecha')";
             	

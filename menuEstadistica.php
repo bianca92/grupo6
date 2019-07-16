0fc58af51c6274f72b95adfa83f7619d8ae6fea0
bianca92 <bianneco92@gmail.com>
2019-07-16 16:43:17 -0300

    menu de estadistica

---
+++ menuEstadistica.php
@@ -0,0 +1,38 @@
+<head>
+  <title>Estadisticas</title>
+  
+<?php
+
+//para que no se pueda acceder a esta pagina si no esta logeado
+
+?>
+</head>
+
+
+
+  
+  
+     <div class="col-md-3" >
+       <div class="panel-group" style="background-color: #33C1FF">
+    <div class="panel panel-default" style="background-color: #33C1FF;font-family: sans-serif;">
+      <div class="panel-heading" style="background-color: #FF7516"><H4><a href= "estadisticas.php">ESTADISTICAS</a></H4></div>
+      <div class="panel-body">
+     
+      <ul class="nav nav-pills nav-stacked" >
+        <li class="active" style="background-color: #33D7FF"></li>
+        <li><a href="gananciasTipo.php">Ganancias por tipo de venta</a></li>
+        <li><a href="gananciasMensuales.php">Ganancias mensuales</a></li>
+     
+        <li><a href="gananciasMensuales.php?ver='premium'">PREMIUM</a></li>
+        <li><a href="gananciasMensuales.php?ver='clasico'">CLASICO</a></li>
+       
+       
+      </ul>
+   </div>
+ </div> </div>
+
+    
+   
+   
+  
+</div>

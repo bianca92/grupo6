eb9041aa7ca33d41d9756a86f7cd89590bd6bb1b
LucyCabrera <lucy.cabrera.ynchausti@gmail.com>
2019-07-16 20:19:58 -0300

    se agregaron botones

--- subastasGanadasUsuario.php
+++ subastasGanadasUsuario.php
@@ -8,6 +8,8 @@ include("cabecera.php");
 include("conexion.php");
 include("mostrarImagen.php");
 include("actualizarSegunFecha.php");
+include("funciones.php");
+$rechazoAutomatico=rechazoAutomatico();
 $con=conectar();
 
   if(isset($_GET['msj'])){
@@ -105,14 +107,48 @@ else{  ?>
             
           <td><h4><?php echo "$"."$pujaMaxima" ?></h4></td>
             <?php
+
+$consulPagado= "SELECT * FROM comprasu WHERE idSubasta=$row[idSubasta]";
+            $resultPagado = mysqli_query($con, $consulPagado);
+            $numPagado = mysqli_num_rows($resultPagado);
+
+            $consultaC= "SELECT * FROM persona WHERE IdPersona='$id'";
+$var_resultadoC = $con->query($consultaC);
+$rowC = mysqli_fetch_array($var_resultadoC);
+//si es el ganador que le permita pagar(si tiene los creditos)
+if($numPagado==0 &&$rowC['credito']>0){
+echo "<td><a href='pagarSemana.php?sub=".$row['idSubasta']."&monto=".$pujaMaxima."&ganador=".$winnerPersona."&tipo=subasta'> <button type='button' class='btn btn-succes'>PAGAR</button> </a></td>" ;
+echo "<td><a href='rechazarSemana.php?sub=".$row['idSubasta']."&monto=".$pujaMaxima."&ganador=".$id."'> <button type='button' class='btn btn-succes'><h5 class=text-danger >RECHAZAR</h5></button> </a></br></td>" ;}
+// SI GANÓ PERO NO TIENE MAS CREDITOS.
+if($numPagado==0 && $rowC['credito']<=0){echo "<td><h4 class=text-danger >NO TIENES MAS CREDITOS</h4></td>";}
+
+//si ya pagó que le diga pagada
+if($numPagado==1){
+  echo "<td><h4 class=text-danger >PAGADA</h4></td>";
+}
+
+
+
             //CALIFICAR SI AUN NO HA CALIFICADO
               $query2 = "SELECT * FROM valoracion WHERE idSubasta=$rowWinner[idSubasta]";
                     $result2 = mysqli_query($con, $query2);
-                    if(mysqli_num_rows($result2)==0){ ?> 
+                    if(mysqli_num_rows($result2)==0 && $numPagado==1){ ?> 
                  
           <td><?php echo "<a href='calificar.php?sub=".$row['idSubasta']."&prop=".$row['idPropiedad']."'> <button type='button' class='btn btn-succes'>CALIFICAR</button> </a></br></td>" ;}?> 
         </tr>  
-         <?php } } }
+         <?php 
+
+
+
+
+
+
+
+
+
+
+
+       } } }
        } //fin del while de la filas de subastas
 ?>      
       </tbody>
